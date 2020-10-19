add1fbb74a6ad3e6953a0110b5dd6eb1f396d246
sidinsomniac <chatterjeesid1993@gmail.com>
2020-10-19 10:24:25 +0530

    Level one authentication

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //jshint esversion:6
 const ejs = require('ejs');
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const port = 3000;
@@ -10,18 +11,52 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+mongoose.connect('mongodb://localhost:27017/userDB', { useNewUrlParser: true, useUnifiedTopology: true });
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+const User = mongoose.model("User", userSchema);
+
 app.get('/', (req, res) => {
     res.render('home');
 });
 
-app.get('/login', (req, res) => {
-    res.render('login');
-});
+app.route('/login')
+    .get((req, res) => {
+        res.render('login');
+    })
+    .post((req, res) => {
+        User.findOne({ email: req.body.username }, (err, foundUser) => {
+            if (!err) {
+                if (foundUser.password === req.body.password) {
+                    res.render('secrets');
+                } else {
+                    res.redirect('/login');
+                }
+            } else {
+                console.log(err);
+            }
+        });
+    });
 
-app.get('/register', (req, res) => {
-    res.render('register');
-});
-
+app.route('/register')
+    .get((req, res) => {
+        res.render('register');
+    })
+    .post((req, res) => {
+        const newUser = new User({
+            email: req.body.username,
+            password: req.body.password
+        });
+        newUser.save(err => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render('secrets');
+            }
+        });
+    });
 app.listen(process.env.PORT || port, () => {
     console.log("Server has started");
 });
